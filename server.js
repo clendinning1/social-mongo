d00868fadb36675c64a1f4b4bc98ed48f756b921
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-04-28 14:58:28 -0500

    updated server.js to prep for new routes and brought in example code for user controller

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const db = require('./config/connection');
-// pull in model
-const { User } = require('./models');
+const routes = require ('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get('/all-users', async (req, res) => {
-    try {
-        // use model in route to find all documents under that schema/model
-        const result = await User.find({});
-        res.status(200).json(result);
-    } catch (err) {
-        res.status(500).send({ message: 'Internal Server Error' })
-    }
-});
+app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
