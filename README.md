8d9e89b29fcf0f96b731816a790ba1cafd07e241
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-05-01 19:38:10 -0500

    cleaned up Thought.js

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 - https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
 - mongoose docs
 - mongodb docs https://www.mongodb.com/docs/manual/
+- ESSENTIAL FOR DATETIME https://www.geeksforgeeks.org/mongoose-schematypes-getters/#
+- AND https://stackoverflow.com/questions/3552461/how-do-i-format-a-date-in-javascript
--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
 
-const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+const dtOptions = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
 
 // parent schema
 const thoughtSchema = new Schema({
@@ -13,8 +13,7 @@ const thoughtSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now,
-        get: (createdAt) => createdAt.toLocaleDateString("en-US", options)
-        // TO DO: use a getter method to format the timestamp on query
+        get: (createdAt) => createdAt.toLocaleDateString("en-US", dtOptions)
     },
     username: { type: String, required: true, },
     reactions: [ reactionSchema ],
@@ -31,27 +30,6 @@ const thoughtSchema = new Schema({
 thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
-
-
-
-
-// TRYING TO BUILD FORMAT DATETIME
-
-// // virtual (formats the date/time)
-// thoughtSchema.virtual('formatDateTime').get(function () {
-//     return this.createdAt.toLocaleDateString("en-US");
-// });
-
-// function formatDT(createdAt) {
-//     return createdAt.toLocaleDateString("en-US");
-// }
-
-// app.get(function(req, res){
-//     return Thought.createdAt.toLocaleDateString("en-US");
-// })
-
-
-
 
 
 // initialize model
